cb605016091ed89551d247cfe2f6ddb0b1c9535d
Natnael Gebremichael <66393884+Natnael92460@users.noreply.github.com>
2022-04-26 20:04:39 +1200

    Update Query4_Task6.sql

--- Project 1/Q4/Query4_Task6.sql
+++ Project 1/Q4/Query4_Task6.sql
@@ -1,17 +1,16 @@
 SELECT Robberid,NickName,NoYears
 FROM Robbers 
-WHERE NoYears > 3;
+WHERE NoYears > 10;
 
 
---  robberid |       nickname       | noyears 
+-- robberid |       nickname       | noyears 
 -- ----------+----------------------+---------
---         2 | Bugsy Malone         |      15
---         3 | Lucky Luchiano       |      15
---         4 | Anastazia            |      15
---         6 | Tony Genovese        |      16
---         7 | Dutch Schulz         |      31
---        11 | Meyer Lansky         |       6
---        15 | Boo Boo Hoff         |      13
---        16 | King Solomon         |      43
---        17 | Bugsy Siegel         |      13
---        20 | Longy Zwillman       |       6
+--        2 | Bugsy Malone         |      15
+--        3 | Lucky Luchiano       |      15
+--        4 | Anastazia            |      15
+--        6 | Tony Genovese        |      16
+--        7 | Dutch Schulz         |      31
+--       15 | Boo Boo Hoff         |      13
+--       16 | King Solomon         |      43
+--       17 | Bugsy Siegel         |      13
+
