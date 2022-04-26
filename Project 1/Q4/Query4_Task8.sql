e976cbe2802ee67d72233507779b3fa1bf88a60d
Natnael Gebremichael <66393884+Natnael92460@users.noreply.github.com>
2022-04-26 19:30:55 +1200

    Update Query4_Task8.sql

--- Project 1/Q4/Query4_Task8.sql
+++ Project 1/Q4/Query4_Task8.sql
@@ -1,1 +1,9 @@
+SELECT Robberid,NickName,
+(Age - NoYears) as years_not_in_prison
+FROM Robbers 
+WHERE NoYears > Age/2;
 
+-- robberid |       nickname       | years_not_in_prison 
+-- ----------+----------------------+---------------------
+--         6 | Tony Genovese        |                  12
+--        16 | King Solomon         |                  31
